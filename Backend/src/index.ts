5ceb53adf393894c6d478706a19242f1f84cbdc6
caiosfg <caio.sfg@gmail.com>
2023-11-01 23:39:28 -0300

    Algumas rotas criadas

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Iniciando')
+import express, { Request, Response } from 'express';
+
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', (request: Request, response: Response) => {
+    return response.status(200).json({ message: 'ok' })
+})
+
+server.post('/logging', (request: Request, response: Response) => {
+    const body = request.body;
+    console.log(body)
+    return response.status(201).json({ message: 'Logging criado' })
+})
+
+server.listen(6000, () => console.log('Running on 6000'))
