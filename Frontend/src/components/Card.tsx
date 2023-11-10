661b00b0658a8f495cc51f41945e350521ab4388
caiosfg <caio.sfg@gmail.com>
2023-11-10 16:32:03 -0300

    Criacao do front

---
+++ Frontend/src/components/Card.tsx
@@ -0,0 +1,9 @@
+export const Card = () => {
+    return (
+      <div className="card">
+        <h1>Alert</h1>
+        <p>Please check de size of mount</p>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+      </div>
+    )
+}
