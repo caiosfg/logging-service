a088c120f0b09775bceb377f79481d64eee21eca
caiosfg <caio.sfg@gmail.com>
2023-11-12 23:38:24 -0300

    Config do styled-component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,36 @@
-import { Card } from "./components/Card";
 import { Layout } from "./components/Layout";
+import styled from "styled-components";
+
+
+const Box = styled.div`
+  background-color: #0A5C36;
+  padding-top: 5px;
+  margin-top: 5px;
+  color: white;
+`
 
 function App() {
   return (
     <Layout>
         <div>
-          <h1>Logging Service</h1>
-          <Card  
-            id={1} 
-            ip= 'test'
-            date = 'test'
-            name = 'test'
-            type ='test'
-            text = 'test'
-          />
+          <Box>
+            <h1>Faça o Login</h1>
+          </Box>
+          <p>Para acesso a plataforma de logs.</p>
+          <label htmlFor="emailInput">
+            Email
+          </label>
+          <input id="emailInput" type="email" />
+
+          <label htmlFor="passwordInput">
+            Senha
+          </label>
+          <input id="passwordInput"  type="password" />
+
+
+          <button>
+            Entrar
+          </button>
         </div>
     </Layout>
   );
